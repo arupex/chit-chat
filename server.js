ae86b9ed1f509b22987901a6cc2b1c501f47d534
Daniel Irwin <dirwin@bluewavesolar.com>
2019-06-05 10:02:25 -0400

    closer to what i need

--- server.js
+++ server.js
@@ -1,37 +1,50 @@
 #!/usr/bin/env node
 
-process.on('uncaughtException', (err, origin) => {
-    console.error('', err.message, err.stack);
-});
-
+process.on('uncaughtException', ({message, stack}) => console.error(message, stack));
 
 const http = require('http');
-
 const fs = require('fs');
+const originalHar = JSON.parse(fs.readFileSync(`${process.argv[2]}`, 'utf8')).log.entries;
 
-const har = JSON.parse(fs.readFileSync(`${process.argv[2]}`, 'utf8')).log.entries;
+const har = JSON.parse(JSON.stringify(originalHar));
 
 const app = http.createServer((req, res) => {
     try {
-        console.log('req', req.method, req.url);
 
-        let harEntityIndex = har.findIndex((entity) => {
-            return entity.request.url.includes(req.url) && req.method === entity.request.method
-        });
+        let findRequest = (entity) => entity.request.url.endsWith(req.url) && req.method === entity.request.method;
+
+        let harEntityIndex = har.findIndex(findRequest);
+        let harEntity;
 
         if(harEntityIndex < 0) {
-            res.statusCode = 404;
-            return res.end('sorry this har file does not contain the request requested');
+            let reverseOriginal = JSON.parse(JSON.stringify(originalHar)).reverse();
+
+            harEntityIndex = reverseOriginal.findIndex(findRequest);
+
+            if (harEntityIndex < 0) {
+                res.statusCode = 404;
+                let msg = 'sorry this har file does not contain the request requested';
+                console.log('req', req.method.padEnd(8, ' '), req.url, msg);
+                return res.end(msg);
+            }
+            else {
+                harEntity = reverseOriginal.splice(harEntityIndex, 1)[0];
+            }
+        }
+        else {
+            harEntity = har.splice(harEntityIndex, 1)[0];
         }
 
-        let harEntity = har.splice(harEntityIndex, 1)[0];
+        console.log('req', req.method.padEnd(8, ' '), req.url);
 
         res.statusCode = harEntity.response.status;
-        harEntity.response.headers.forEach(({name, value}) => {
-            res.setHeader(name, value)
-        });
+        harEntity.response.headers.forEach(({name, value}) => res.setHeader(name, value));
+        res.setHeader('Date', (new Date()).toUTCString());
 
-        res.end(harEntity.response.content.text);
+        setTimeout(() => {
+            res.end(harEntity.response.content.text);
+        }, req.method !== 'OPTIONS' ? (parseInt(process.argv[4]) || 10) : 10);
+
     }
     catch(e) {
         console.error('Error!', e.message, e.stack);
